c35de1d71c3d98c348bd77c0968b4657bf0c740e
s28600 <s28600@pjwstk.edu.pl>
2024-03-18 12:58:08 +0100

    Base container added

---
+++ APDB_C03/APDB_C03/Containers/BaseContainer.cs
@@ -0,0 +1,23 @@
+﻿namespace APDB_C03.Containers;
+
+public class BaseContainer
+{
+    protected double MaxLoad { get; }
+    protected double Weigth { get; }
+    protected double Heigth { get; }
+    protected double Depth { get; }
+    protected double Load { get; set; }
+    protected string SerialNum { get; }
+
+    public BaseContainer(double maxLoad, double weigth, double heigth, double depth, double load, string serialNum)
+    {
+        MaxLoad = maxLoad;
+        Weigth = weigth;
+        Heigth = heigth;
+        Depth = depth;
+        Load = load;
+        SerialNum = serialNum;
+    }
+    
+    
+}
